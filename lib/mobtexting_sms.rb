4236dccf858a48af03dd0eb650f97a354e13a86e
Vishal <vishal.n@mobtexting.com>
2019-03-20 11:14:38 +0530

    verify added

--- lib/mobtexting_sms.rb
+++ lib/mobtexting_sms.rb
@@ -17,4 +17,25 @@ module MobtextingSms
 		end
 	end
 
+	class Verify
+		def initialize(access_token)
+			@access_token = access_token
+		end
+		def send(to)
+			api_endpoint = 'https://portal.mobtexting.com/api/v2/'
+			url = api_endpoint + '/verify?to=' + to
+
+			url = URI.parse(url)
+			https = Net::HTTP.new(url.host,url.port)
+			https.use_ssl = true
+			req = Net::HTTP::Post.new(url)
+
+			req['Accept'] = 'application/json'
+			req['Authorization'] = @access_token
+			res = https.request(req)
+
+			return res.body
+		end
+	end
+
 end
